b48ff13c669d3e08b62578592cb589bd6e7c4d46
Serhii <124461383+nhnserg@users.noreply.github.com>
2023-12-07 20:50:28 +0100

    fix

--- src/pages/Movies/Movies.styled.jsx
+++ src/pages/Movies/Movies.styled.jsx
@@ -1,5 +1,25 @@
 import styled from 'styled-components';
 
-export const GoBackBtn = styled.button``;
-export const AdInfo = styled.p``;
-export const InfoList = styled.ul``;
+export const Main = styled.main`
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 20px;
+`;
+
+export const SectionTitle = styled.h2`
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 20px;
+`;
+
+export const MoviesListContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  gap: 20px;
+`;
+
+export const NoResults = styled.p`
+  font-size: 16px;
+  color: #555;
+  margin-top: 20px;
+`;
