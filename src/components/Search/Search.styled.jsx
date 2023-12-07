d002a0647081af55c93799437fb0a4809b9ba2a8
Serhii <124461383+nhnserg@users.noreply.github.com>
2023-12-07 20:25:29 +0100

    Update Search.styled.jsx

--- src/components/Search/Search.styled.jsx
+++ src/components/Search/Search.styled.jsx
@@ -3,30 +3,34 @@ import styled from 'styled-components';
 export const SearchForm = styled.form`
   display: flex;
   margin-bottom: 20px;
+  max-width: 400px;
+  margin: 20px auto;
 `;
 
 export const SearchFormInput = styled.input`
-  padding: 10px;
+  flex: 1;
+  padding: 12px;
   font-size: 16px;
   border: 1px solid #ccc;
-  border-radius: 4px;
-  margin-right: 10px;
-  flex: 1;
+  border-radius: 6px 0 0 6px;
+  transition: border-color 0.3s ease-in-out;
 
   &:focus {
     outline: none;
     border-color: #007bff;
+    box-shadow: 0 0 5px rgba(0, 123, 255, 0.5);
   }
 `;
 
 export const SearchBtn = styled.button`
-  padding: 10px 20px;
+  padding: 12px 20px;
   font-size: 16px;
   background-color: #007bff;
   color: #fff;
-  border: none;
-  border-radius: 4px;
+  border: 1px solid #007bff;
+  border-radius: 0 6px 6px 0;
   cursor: pointer;
+  transition: background-color 0.3s ease-in-out;
 
   &:hover {
     background-color: #0056b3;
